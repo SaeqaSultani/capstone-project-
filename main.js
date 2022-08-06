b93f9e74fd8bd46a16cf2181e62294087b87dc08
Saeqa Sultani <saeqasultani@gmail.com>
2022-08-06 20:12:59 +0430

    Edite the path of images in the js files

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const arr = [
   {
     profile: './image/java.png',
     name: 'JAVA',
-    dec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    dec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   },
 ];
 
